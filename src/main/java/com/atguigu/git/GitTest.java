e24aef797ca5a04c6a4854ba6a080cb6a2ee3d36
qiuzhi <772370182@qq.com>
2024-04-14 14:38:53 +0800

    hot-fix第一次提交

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -17,11 +17,5 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
-        System.out.println("master test!");
-        System.out.println("hot-fix test!");
-        System.out.println("push test!");
-        System.out.println("pull test!");
-        System.out.println("gitee test!");
-        System.out.println("gitee test2!");
     }
 }
