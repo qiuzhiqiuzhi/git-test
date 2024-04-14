d6a2df8fc0d8c9b7861e56c09b0dd0d5709fc6a6
qiuzhi <772370182@qq.com>
2024-04-14 15:04:41 +0800

    master提交

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -23,6 +23,6 @@ public class GitTest {
         System.out.println("hello git5!");
         System.out.println("hello git6!");
         System.out.println("这里是hot-fix分支代码");
-        System.out.println("这里是master分支代码");
+        System.out.println("这里是master分支代码2222222222222222");
     }
 }
