a0b7e8da307783916875814c34f2c7bf8bbe03a8
qiuzhiqiuzhi <110347376+qiuzhiqiuzhi@users.noreply.github.com>
2024-04-14 16:06:56 +0800

    11111

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -26,5 +26,7 @@ public class GitTest {
         System.out.println("master push test");
         System.out.println("master push tes2");
         System.out.println("master push tes3");
+        System.out.println("master push tes4");
+        
     }
 }
