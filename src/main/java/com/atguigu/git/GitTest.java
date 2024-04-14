a334976adfe60e4b91ed44462163f3f2d477d1ce
qiuzhi <772370182@qq.com>
2024-04-14 09:29:18 +0800

    第二次提交本地库

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,7 +2,11 @@ package com.atguigu.git;
 
 /**
  * @ClassName GitTest
- * @Description TODO
+ * @Description 新创建的一个文件为红色表示未被git追踪到；
+ * 添加到暂存区之后文件名变绿色，提交到本地库颜色恢复正常；
+ * 修改提交到本地库的一个文件颜色会变蓝色；
+ * 因为蓝色的文件已经被git追踪到，可以直接提交到本地库；
+ *
  * @Author @仇职
  * @Date 2024-04-14 9:17
  * @Version 1.0
@@ -12,6 +16,8 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
+
+
         System.out.println("hello git4!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
