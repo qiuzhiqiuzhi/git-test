7dd21bcaaaf576030106cf22b3368d025149b001
qiuzhi <772370182@qq.com>
2024-04-14 09:23:12 +0800

    第一次提交本地库

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,23 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author @仇职
+ * @Date 2024-04-14 9:17
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+        System.out.println("master test!");
+        System.out.println("hot-fix test!");
+        System.out.println("push test!");
+        System.out.println("pull test!");
+        System.out.println("gitee test!");
+        System.out.println("gitee test2!");
+    }
+}
