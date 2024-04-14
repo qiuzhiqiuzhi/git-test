2f9000d352cd674d018df13c5d7664dcbf25ade8
qiuzhi <772370182@qq.com>
2024-04-14 15:10:38 +0800

    hot-fix提交

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -25,5 +25,6 @@ public class GitTest {
         System.out.println("这里是hot-fix分支代码");
         System.out.println("这里是hot-fix分支代码22222222222222");
         System.out.println("这里是hot-fix分支代码3333333333333333");
+        System.out.println("这里是hot-fix分支代码4444444444444444");
     }
 }
